f6ff8066b66501614e09fa31525ad23830189960
ReztuZt <restusetiawan948@gmail.com>
2024-01-05 04:13:51 +0700

    footer fix

--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -2,12 +2,13 @@
 </div>
 <!-- /.content -->
 </div>
+</div>
 <!-- /.content-wrapper -->
 <footer class="main-footer">
     <div class="float-right d-none d-sm-block">
         <b>Version</b> 3.2.0
     </div>
-    <strong>Copyright &copy; 2014-2021 <a href="https://adminlte.io">AdminLTE.io</a>.</strong> All rights reserved.
+    <strong>Copyright &copy; 2023-2024 <a href="https://manimonki.studio">Gomonki</a>.</strong> R2
 </footer>
 
 <!-- Control Sidebar -->
