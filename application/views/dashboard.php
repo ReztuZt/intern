98693c8a3ab1b46eb03a30a296bb7e5651c4182a
ReztuZt <restusetiawan948@gmail.com>
2024-01-05 16:49:39 +0700

    dashboard

--- application/views/dashboard.php
+++ application/views/dashboard.php
@@ -86,19 +86,21 @@ $result = $conn->query($sql);
               </div>
               <!-- ./col -->
               <div class="card">
-    <div class="card-header border-0">
-        <h3 class="card-title">Courses</h3>
-        <div class="card-tools">
-            <a href="#" class="btn btn-tool btn-sm">
-                <i class="fas fa-download"></i>
-            </a>
-            <a href="#" class="btn btn-tool btn-sm">
-                <i class="fas fa-bars"></i>
-            </a>
-        </div>
-    </div>
-    <div class="card-body table-responsive p-0">
-        <table class="table table-striped table-valign-middle">
+              <div class="card-header border-transparent">
+                <h3 class="card-title">Course</h3>
+
+                <div class="card-tools">
+                  <button type="button" class="btn btn-tool" data-card-widget="collapse">
+                    <i class="fas fa-minus"></i>
+                  </button>
+                 
+                  </button>
+                </div>
+              </div>
+              <!-- /.card-header -->
+              <div class="card-body p-0">
+                <div class="table-responsive">
+                  <table class="table m-0">
             <thead>
                 <tr>
                     <th>Nama Course</th>
